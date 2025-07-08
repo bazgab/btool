8030dc1372a1d09a9faab5ccad894a26dab6a0a0
Gabriel Baz <gabrielbzzt@gmail.com>
2025-07-08 10:30:44 -0300

    add LICENSE file
    add .gitignore to avoid .idea/ of being commited

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 /*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+DISCLAIMER:
 
+btool is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation
+
+btool is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.
+
+The full license can be seen in the file ./LICENSE.  If not see
+<http://www.gnu.org/licenses/>.
 */
 package main
 
