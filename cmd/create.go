e7d10adbab5841830cb10a82c7dd6095c91f7945
Gabriel Bazzotti <gabrielbzzt@gmail.com>
2025-08-22 12:55:18 -0300

    tests(cmd): add tests for create output

--- cmd/create.go
+++ cmd/create.go
@@ -22,7 +22,7 @@ import (
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
 	"log/slog"
-	"os/exec"
+	//"os/exec"
 	"os"
 	"strings"
 )
@@ -79,37 +79,30 @@ func runCreate(cmd *cobra.Command, _ []string) {
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-
-	/* Testing for multiple values 
-	for i := 0; i < len(confValues.Dump.DatabaseName); i++ {
-		fmt.Printf("Database name %d : %s\n", i, confValues.Dump.DatabaseName[i])
-
-	}
-	*/
-	
-	// Design Issue:
-	// overcomplicating by having default values. 
-	
-	// An approach to not have multiple values laying around:
 	
 	// Perform a basic dump
 	
-	if confValues.Dump.Type != all_databases {
-		if confValues.Dump.Tables != all_tables {
+	uL := confValues.Database.User
+	uP := confValues.Database.Password 
+	loginDetails := "--user=" + uL + " " + "-p" + uP
+	
+	if confValues.Dump.Type != "all_databases" {
+		if confValues.Dump.Tables[0] != "all_tables" {
 			
 		// Runs the command with database_name and specific table name
 		
 		for i := 0; i < len(confValues.Dump.DatabaseName); i++ {
 		fmt.Printf("Creating dump for : %s\n", confValues.Dump.DatabaseName[i])
 		dumpName := confValues.Dump.DatabaseName[i] + "-dump.sql"
-		arg := []string{"--databases", confValues.Dump.DatabaseName[i], confValues.Dump.Tables[i], ">", dumpName }
+		arg := []string{loginDetails, "--databases", confValues.Dump.DatabaseName[i], confValues.Dump.Tables[i], ">", dumpName }
 		fmt.Sprintf("Running command with following argument: %s", strings.Join(arg, " "))
-		out, err := exec.Command("usr/bin/mariadb-dump", strings.Join(arg, " ")).Output()
+		fmt.Println("usr/bin/mariadb-dump", strings.Join(arg, " "));
+		// out, err := exec.Command("usr/bin/mariadb-dump", strings.Join(arg, " ")).Output()
 		if err != nil {
 			slog.Info("Error when executing dump command")
 			os.Exit(1)
 		} 
-		fmt.Printf("Output: %s", out)
+		// fmt.Printf("Output: %s", out)
 		}
 		
 		} else {
@@ -118,89 +111,35 @@ func runCreate(cmd *cobra.Command, _ []string) {
 			for i := 0; i < len(confValues.Dump.DatabaseName); i++ {
 			fmt.Printf("Creating dump for : %s\n", confValues.Dump.DatabaseName[i])
 			dumpName := confValues.Dump.DatabaseName[i] + "-dump.sql"
-			arg := []string{"--databases", confValues.Dump.DatabaseName[i], ">", dumpName }
+			arg := []string{loginDetails, "--databases", confValues.Dump.DatabaseName[i], ">", dumpName }
 			fmt.Sprintf("Running command with following argument: %s", strings.Join(arg, " "))
-			out, err := exec.Command("usr/bin/mariadb-dump", strings.Join(arg, " ")).Output()
+			fmt.Println("usr/bin/mariadb-dump", strings.Join(arg, " "));
+			// out, err := exec.Command("usr/bin/mariadb-dump", strings.Join(arg, " ")).Output()
 			if err != nil {
 			slog.Info("Error when executing dump command")
 			os.Exit(1)
-			} 
+			}
+			// fmt.Printf("Output: %s", out); 
 		}
 		
+		} 
 	} else {
+		
 		// Config for running command with "--all-databases" hardcoded
 		for i := 0; i < len(confValues.Dump.DatabaseName); i++ {
 		fmt.Printf("Creating dump for : %s\n", confValues.Dump.DatabaseName[i])
 		dumpName := confValues.Dump.DatabaseName[i] + "-dump.sql"
-		arg := []string{"--all-databases", ">", dumpName }
+		arg := []string{loginDetails ,"--all-databases", ">", dumpName }
 		fmt.Sprintf("Running command with following argument: %s", strings.Join(arg, " "))
-		out, err := exec.Command("usr/bin/mariadb-dump", strings.Join(arg, " ")).Output()
+		fmt.Println("usr/bin/mariadb-dump", strings.Join(arg, " "));
+		// out, err := exec.Command("usr/bin/mariadb-dump", strings.Join(arg, " ")).Output()
 		if err != nil {
 			slog.Info("Error when executing dump command")
 			os.Exit(1)
 		} 
-		fmt.Printf("Output: %s", out)
+		// fmt.Printf("Output: %s", out)
 		}
 	}
-	
-	
-	
-	/*
-	// Firstly check the correct usage of db engine
-	if confValues.Database.Engine == "mariadb" {
-		slog.Info("MariaDB selected - proceeding setup")
-	} else {
-		slog.Error("Unsupported Database Engine")
-		os.Exit(1)
-	}
-
-	//Check values, if not > parse defaults
-	if confValues.Database.User == "" {
-		slog.Info("User check - No selected user. Setting option to default value: ", defaultUser)
-	}	
-	slog.Info(fmt.Sprintf("User: %s\n", confValues.Database.User))
-	
-	if confValues.Database.Password == "" {
-		slog.Info("Password check - No password entered. Password is a \033[1mrequired field\033[0m, for usage see the docs.")
-		// TODO > if no password selected, panic
-	} 
-	
-	if confValues.Database.Host == "" {
-		slog.Info(fmt.Sprintf("Host check - No selected host. Setting option to default value: %s", defaultHost))
-	}	
-	
-	if confValues.Dump.Path == "" {
-		slog.Info("Path check - No path entered. Path is a \033[1mrequired field\033[0m, for usage see the docs.")
-		// TODO > if no path selected, panic
-	} 
-	slog.Info(fmt.Sprintf("Creating dump on path: %s", confValues.Dump.Path))
-	
-	slog.Info("Type check: ")
-	
-	/* A better aproach
-	
-	if confValues.Dump.Type != all_databases {
-		if confValues.Dump.Tables != all_tables {
-		Config for running database_name in command with specific table name
-		} else {
-			Config for running database_name in command
-		}
-		
-	} else {
-		Config for running command with "--all-databases" hardcoded
-	}
-	
-	
-	if confValues.Dump.Path == "" {
-		slog.Info(fmt.Sprintf("Tables check - No tables entered. Setting option to default value: %s", defaultTables))
-		// TODO > if no path selected, panic
-	} 
-
-	// Perform dump
-	// Added on top for testing functionality 
-	
-	*/
-
-	
 	
 }
+
